3bfaf52e45d16a68a75683c64506e7e13f3dd010
GisenTheCat <GisenSpelar@gmail.com>
2026-05-22 09:57:16 +0200

    i forgot to commit stuff

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
+window.submissions = window.localStorage.getItem("submissions") ?? 0;
+
 window.addEventListener("DOMContentLoaded", () => {
     const confessionTextArea = document.getElementById("confession");
     const submitButton = document.getElementById("submit");
 
-    submitButton.addEventListener("click", submit);
+    submitButton.addEventListener("click", showSubmitConfirmation);
     confessionTextArea.addEventListener("keypress", (event) => {
         if (event.key === "Enter") {
             event.preventDefault();
-            submit();
+            showSubmitConfirmation();
+        } else {
+            submitButton.ariaDisabled = confessionTextArea.value.trim() === "";
         }
     });
 
-    function submit() {}
+    function showSubmitConfirmation() {
+        if (confessionTextArea.value.trim() === "") return;
+        if (confirm("Are you sure you want to submit this confession?")) {
+            submit();
+        }
+    }
+
+    function submit() {
+        window.submissions++;
+        window.localStorage.setItem("submissions", window.submissions);
+        confessionTextArea.value = "";
+    }
 });
